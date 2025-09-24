fb9bf17ed1fa64e77c06b67d5ab15f818abea556
Marcus Radder <marcusradder@gmail.com>
2025-09-24 08:48:39 -0400

    Fix path on profile pic for preview

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/svelte-portfolio' : ''
+			base: process.env.NODE_ENV === 'production' ? '/about' : ''
 		}
 	},
 	extensions: ['.svelte', '.svx']
